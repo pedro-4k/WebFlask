00725056f89ed70dfaddc88a81e483166ab01f2c
PEDRO LOBATO <pedro.jr.py@hotmail.com>
2025-04-11 17:03:11 -0300

    Commit

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-from Flask import Flask
+from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Olá, Flask!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
